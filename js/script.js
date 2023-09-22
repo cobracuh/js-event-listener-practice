d0bfa1a662f4b10400de210ef8e57a3c08549adc
brittany_frisby <befrisby.design@gmail.com>
2023-09-22 10:22:12 -0500

    Added event listener functions

--- js/script.js
+++ js/script.js
@@ -3,5 +3,14 @@ let button = document.querySelector('.show-cat');
 let cat = document.querySelector('.cat');
 
 button.addEventListener('click', function () {
-  cat.classList.add('show');
+  if (cat.classList.contains('show')) {
+    // console.log('Yes');
+    button.innerText = 'Wait, come back';
+    button.classList.add('disappear');
+  } else {
+    // console.log('No');
+    button.innerText = 'Shoo, cat!!!';
+    cat.classList.add('show');
+    button.classList.remove('disappear');
+  }
 });
